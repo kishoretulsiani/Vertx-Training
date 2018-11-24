88b82d9cb5215712fcf98bc989e9610492acbcec
Kishore Tulsiani <kishore.tulsiani1@aexp.com>
2018-11-24 12:43:04 -0700

    adding hello-world

---
+++ README.md
@@ -0,0 +1,8 @@
+## Vert.x Training
+
+This project shows example which helps understand Vert.x framework.
+
+To build project
+
+    mvn clean install
+
---
+++ hello-world/src/main/java/io/vertx/example/HelloWorld.java
@@ -0,0 +1,19 @@
+package io.vertx.example;
+
+import io.vertx.core.Vertx;
+
+/**
+ * @author Kishore Tulsiani
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    // Create an HTTP server which simply returns "Hello World!" to each request.
+    Vertx.vertx()
+      .createHttpServer()
+      .requestHandler(req -> req.response().end("Hello World!"))
+      .listen(8080);
+
+  }
+}
